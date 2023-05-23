b5e74e4ff9f3ce728ad195369764b0513fbddb69
root <root@ip-172-31-24-128.ec2.internal>
2023-05-23 17:58:48 +0000

    git

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'admin' );
 define( 'DB_PASSWORD', 'Salman9614$' );
 
 /** Database hostname */
-define( 'DB_HOST', 'database-2.cssjsx9cgosf.us-east-1.rds.amazonaws.com' );
+define( 'DB_HOST', 'database-1.cssjsx9cgosf.us-east-1.rds.amazonaws.com' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
